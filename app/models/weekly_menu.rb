345506e62858a6e115017150e3e63ee0aa3eae45
Odell <odell.purifoy@gmail.com>
2019-01-08 13:20:13 -0500

    Styling and controller updates

--- app/models/weekly_menu.rb
+++ app/models/weekly_menu.rb
@@ -3,9 +3,4 @@ class WeeklyMenu < ApplicationRecord
   attr_readonly :available_day
   has_and_belongs_to_many :meals
   validates :meals, presence: true
-
-  def copy_week
-    last_week = WeeklyMenu.last
-    new_week = WeeklyMenu.new(last_week.attributes.slice(*WeeklyMenu.attribute_names))
-  end
 end
